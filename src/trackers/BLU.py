12e8f42ca63eac8b5776c9f84b9bc73f4422cbf2
L4GSP1KE <email>
2021-06-10 22:12:02 -0600

    thanks squatchy

--- src/trackers/BLU.py
+++ src/trackers/BLU.py
@@ -34,7 +34,7 @@ class Blu():
         if meta['bdinfo'] != None:
             mi_dump = None
         else:
-            mi_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'r').read()
+            mi_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'r', encoding='utf-8').read()
         desc = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/DESCRIPTION.txt", 'r').read()
         open_torrent = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[BLU]{meta['clean_name']}.torrent", 'rb')
         files = {'torrent': open_torrent}
