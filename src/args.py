a3847e89b5a96e7f0b0e3471dbffb7fc3247ea41
L4GSP1KE <email>
2021-07-31 11:44:13 -0600

    clearer description for -client

--- src/args.py
+++ src/args.py
@@ -48,7 +48,7 @@ class Args():
         parser.add_argument('-m', '--manual', action='store_true', required=False, help="Manual Mode. Returns link to ddl screens/base.torrent")
         parser.add_argument('-nh', '--nohash', action='store_true', required=False, help="Don't hash .torrent")
         parser.add_argument('-dr', '--draft', action='store_true', required=False, help="Send to drafts (BHD)")
-        parser.add_argument('-client', '--client', nargs='*', required=False, help="Override default torrent client")
+        parser.add_argument('-client', '--client', nargs='*', required=False, help="Use this torrent client instead of default")
         parser.add_argument('-tk', '--trackers', nargs='*', required=False, help="Upload to these trackers")
         parser.add_argument('-ua', '--unattended', action='store_true', required=False, help=argparse.SUPPRESS)
         
