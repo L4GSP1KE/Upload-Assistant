14054c94045975b34adbd93b9234041a48dbe4cf
L4GSP1KE <email>
2022-01-24 12:20:38 -0700

    fix typo

--- src/trackers/UHDHEAVEN.py
+++ src/trackers/UHDHEAVEN.py
@@ -140,7 +140,7 @@ class UHDHEAVEN():
         
     async def edit_desc(self, meta):
         base = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/DESCRIPTION.txt", 'r').read()
-        with open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[UHDHEAVEN]]DESCRIPTION.txt", 'w') as desc:
+        with open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[UHDHEAVEN]DESCRIPTION.txt", 'w') as desc:
             desc.write(base)
             images = meta['image_list']
             if len(images) > 0: 
