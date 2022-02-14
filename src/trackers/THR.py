df7915587dedb3d8ef0a99dc3ed829db0e4d8544
L4GSP1KE <email>
2022-02-14 16:27:48 -0700

    add full mediainfo if use pronfo

--- src/trackers/THR.py
+++ src/trackers/THR.py
@@ -207,6 +207,11 @@ class THR():
 
             for each in image_list:
                 desc.write(f"\n[img]{each}[/img]\n")
+            if pronfo:
+                with open(os.path.abspath(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt"), 'r') as mi_file:
+                    full_mi = mi_file.read()
+                    desc.write(f"[/align]\n[hide=FULL MEDIAINFO]{full_mi}[/hide][align=center]")
+                    mi_file.close()
             desc.write("\n\n[size=2][url=https://www.torrenthr.org/forums.php?action=viewtopic&topicid=8977]Created by L4G's Upload Assistant[/url][/size][/align]")
             desc.close()
         return pronfo
