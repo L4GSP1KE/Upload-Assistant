39ab7fdfac111f2bbc4d17b34cd46acaefe8cc71
L4GSP1KE <email>
2022-07-05 22:56:42 -0600

    use more agressive compression on images larger than 31MB

--- src/prep.py
+++ src/prep.py
@@ -933,7 +933,10 @@ class Prep():
                     pyver = platform.python_version_tuple()
                     if int(pyver[0]) == 3 and int(pyver[1]) >= 7:
                         import oxipng 
-                    oxipng.optimize(image, level=1)
+                    if os.path.getsize(image) >= 31000000:
+                        oxipng.optimize(image, level=6)
+                    else:
+                        oxipng.optimize(image, level=1)
                 except:
                     pass
         return
