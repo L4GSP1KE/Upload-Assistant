970850ae75b44286d7b8d5512ac4b32d3c7e7bdb
L4GSP1KE <email>
2022-08-23 12:38:15 -0600

    keep service names

--- src/trackers/FL.py
+++ src/trackers/FL.py
@@ -75,8 +75,6 @@ class FL():
 
     async def edit_name(self, meta):
         fl_name = meta['name']
-        if meta.get('source', '').upper() == 'WEB' and meta.get('service', '').strip() != '':
-            fl_name = fl_name.replace(f"{meta.get('service', '')} ", '')
         if 'DV' in meta.get('hdr', ''):
             fl_name = fl_name.replace(' DV ', ' DoVi ')
         if meta.get('type') in ('WEBDL', 'WEBRIP', 'ENCODE'):
