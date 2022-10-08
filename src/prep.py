a63e02bf81635fe173c007e5f4841a8fe6f60d03
L4GSP1KE <email>
2022-10-08 16:14:48 -0600

    fix imdb arg not working

--- src/prep.py
+++ src/prep.py
@@ -93,7 +93,7 @@ class Prep():
         
         # If BD:
         if meta['is_disc'] == "BDMV":
-            video, meta['scene'], meta['imdb'] = self.is_scene(meta['path'])
+            video, meta['scene'], meta['imdb'] = self.is_scene(meta['path'], meta.get('imdb', None))
             meta['filelist'] = []
             try:
                 guess_name = bdinfo['title'].replace('-',' ')
@@ -121,7 +121,7 @@ class Prep():
             mi_dump = None
         #IF DVD
         elif meta['is_disc'] == "DVD":
-            video, meta['scene'], meta['imdb'] = self.is_scene(meta['path'])
+            video, meta['scene'], meta['imdb'] = self.is_scene(meta['path'], meta.get('imdb', None))
             meta['filelist'] = []
             guess_name = meta['discs'][0]['path'].replace('-',' ')
             # filename = guessit(re.sub("[^0-9a-zA-Z]+", " ", guess_name))['title']
@@ -142,7 +142,7 @@ class Prep():
             meta['resolution'] = self.get_resolution(guessit(video), meta['uuid'], base_dir)
             meta['sd'] = self.is_sd(meta['resolution'])
         elif meta['is_disc'] == "HDDVD":
-            video, meta['scene'], meta['imdb'] = self.is_scene(meta['path'])
+            video, meta['scene'], meta['imdb'] = self.is_scene(meta['path'], meta.get('imdb', None))
             meta['filelist'] = []
             guess_name = meta['discs'][0]['path'].replace('-','')
             filename = guessit(guess_name)['title']
@@ -166,7 +166,7 @@ class Prep():
         #If NOT BD/DVD/HDDVD
         else:
             videopath, meta['filelist'] = self.get_video(videoloc, meta.get('mode', 'discord')) 
-            video, meta['scene'], meta['imdb'] = self.is_scene(videopath)
+            video, meta['scene'], meta['imdb'] = self.is_scene(videopath, meta.get('imdb', None))
             guess_name = ntpath.basename(video).replace('-',' ')
             filename = guessit(re.sub("[^0-9a-zA-Z\[\]]+", " ", guess_name))["title"]
             untouched_filename = os.path.basename(video)
@@ -612,8 +612,7 @@ class Prep():
     """
     Is a scene release?
     """
-    def is_scene(self, video):
-        imdb = None
+    def is_scene(self, video, imdb=None):
         scene = False
         base = os.path.basename(video)
         base = os.path.splitext(base)[0]
