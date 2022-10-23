76829d282d58fab42ae8bea39ed5e5829a5a5744
L4GSP1KE <email>
2022-10-23 14:18:00 -0600

    add season/episode to arg override list

--- upload.py
+++ upload.py
@@ -127,7 +127,7 @@ async def do_the_thing(base_dir):
             with open(f"{base_dir}/tmp/{os.path.basename(path)}/meta.json") as f:
                 saved_meta = json.load(f)
                 for key, value in saved_meta.items():
-                    overwrite_list = ['trackers', 'dupe', 'debug', 'anon', 'category', 'type', 'screens', 'nohash', 'manual_edition', 'imdb', 'tmdb_manual', 'mal', 'manual', 'hdb', 'ptp', 'blu', 'no_aka', 'no_year', 'no_dub', 'client', 'desclink', 'descfile', 'desc', 'draft', 'region', 'freeleech', 'personalrelease', 'unattended']
+                    overwrite_list = ['trackers', 'dupe', 'debug', 'anon', 'category', 'type', 'screens', 'nohash', 'manual_edition', 'imdb', 'tmdb_manual', 'mal', 'manual', 'hdb', 'ptp', 'blu', 'no_aka', 'no_year', 'no_dub', 'client', 'desclink', 'descfile', 'desc', 'draft', 'region', 'freeleech', 'personalrelease', 'unattended', 'season', 'episode']
                     if meta.get(key, None) != value and key in overwrite_list:
                         saved_meta[key] = meta[key]
                 meta = saved_meta
