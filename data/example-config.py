a3a242ba9b8ab0c6cccb0181ec5e285782f7b9e1
L4GSP1KE <email>
2022-10-24 18:36:44 -0600

    -tdb +pter in default trackers

--- data/example-config.py
+++ data/example-config.py
@@ -24,7 +24,7 @@ config = {
 
     "TRACKERS" : {
         # Which trackers do you want to upload to?
-        "default_trackers" : "BLU, BHD, AITHER, STC, STT, SN, THR, R4E, HP, ACM, PTP, LCD, LST, TDB",
+        "default_trackers" : "BLU, BHD, AITHER, STC, STT, SN, THR, R4E, HP, ACM, PTP, LCD, LST, PTER",
 
         "BLU" : {
             "useAPI" : False, # Set to True if using BLU
