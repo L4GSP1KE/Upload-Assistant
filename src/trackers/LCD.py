008d9f4e80380225da481dae44c825fbf18bc2f0
costaht <50637431+costaht@users.noreply.github.com>
2022-12-10 15:57:36 -0500

    Updating LCD URL
    We're having a DNS problem and we are temporarily using a new name.

--- src/trackers/LCD.py
+++ src/trackers/LCD.py
@@ -22,9 +22,9 @@ class LCD():
         self.config = config
         self.tracker = 'LCD'
         self.source_flag = 'LOCADORA'
-        self.search_url = 'https://locadora.xyz/api/torrents/filter'
-        self.torrent_url = 'https://locadora.xyz/api/torrents/'
-        self.upload_url = 'https://locadora.xyz/api/torrents/upload' 
+        self.search_url = 'https://locadora.cc/api/torrents/filter'
+        self.torrent_url = 'https://locadora.cc/api/torrents/'
+        self.upload_url = 'https://locadora.cc/api/torrents/upload' 
         self.signature = f"\n[center]Criado usando L4G's Upload Assistant[/center]"
         
         pass
