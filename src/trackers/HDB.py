d063015f31ca9d475f9c87c799c08ced5676881b
L4GSP1KE <email>
2023-06-04 20:51:39 -0600

    HDB: naming tweaks

--- src/trackers/HDB.py
+++ src/trackers/HDB.py
@@ -178,7 +178,7 @@ class HDB():
         if 'HDR' in meta.get('hdr', ''):
             if 'HDR10+' not in meta['hdr']:
                 hdb_name = hdb_name.replace('HDR', 'HDR10')
-        if meta.get('type') in ('WEBDL', 'WEBRIP', 'ENCODE'):
+        if meta.get('type') in ('WEBDL', 'WEBRIP', 'ENCODE') and "DD" in meta['audio']:
             hdb_name = hdb_name.replace(meta['audio'], meta['audio'].replace(' ', '', 1).replace('Atmos', ''))
         else:
             hdb_name = hdb_name.replace(meta['audio'], meta['audio'].replace('Atmos', ''))
@@ -192,8 +192,8 @@ class HDB():
         hdb_name = hdb_name.replace('Dubbed', '').replace('Dual-Audio', '')
         hdb_name = hdb_name.replace('REMUX', 'Remux')
         hdb_name = ' '.join(hdb_name.split())
-        hdb_name = re.sub("[^0-9a-zA-ZÀ-ÿ. &+'\-\[\]]+", "", hdb_name)
-        hdb_name = hdb_name.replace(' ', '.').replace('..', '.')
+        hdb_name = re.sub("[^0-9a-zA-ZÀ-ÿ. :&+'\-\[\]]+", "", hdb_name)
+        hdb_name = hdb_name.replace(' .', '.').replace('..', '.')
 
         return hdb_name 
 
