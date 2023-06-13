d52f68b11669a8c3c15aea6734d4beced1e4a6b7
L4GSP1KE <email>
2023-06-12 22:33:04 -0600

    Update example config

--- data/example-config.py
+++ data/example-config.py
@@ -34,19 +34,19 @@ config = {
 
     "TRACKERS" : {
         # Which trackers do you want to upload to?
-        "default_trackers" : "BLU, BHD, AITHER, STC, STT, SN, THR, R4E, HP, ACM, PTP, LCD, LST, PTER, NBL, ANT",
+        "default_trackers" : "BLU, BHD, AITHER, STC, STT, SN, THR, R4E, HP, ACM, PTP, LCD, LST, PTER, NBL, ANT, MTV",
 
         "BLU" : {
             "useAPI" : False, # Set to True if using BLU
             "api_key" : "BLU api key",
             "announce_url" : "https://blutopia.cc/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
         "BHD" : {
             "api_key" : "BHD api key",
             "announce_url" : "https://beyond-hd.me/announce/customannounceurl",
             "draft_default" : "True",
-            # "anon" : "False"
+            # "anon" : False
         },
         "PTP" : {
             "useAPI" : False, # Set to True if using PTP
@@ -60,35 +60,34 @@ config = {
         "AITHER" :{
             "api_key" : "AITHER api key",
             "announce_url" : "https://aither.cc/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
         "R4E" :{
             "api_key" : "R4E api key",
             "announce_url" : "https://racing4everyone.eu/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
         "HUNO" : {
             "api_key" : "HUNO api key",
             "announce_url" : "https://hawke.uno/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
 	    "MTV": {
             'api_key' : 'get from security page',
             'username' : '<USERNAME>',
             'password' : '<PASSWORD>',
             "announce_url": "get from https://www.morethantv.me/upload.php",
-            # not currently implemented
-            # "anon" : "False"
+            "anon" : False
         },
         "STC" :{
             "api_key" : "STC",
             "announce_url" : "https://skipthecommericals.xyz/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
         "STT" :{
             "api_key" : "STC",
             "announce_url" : "https://stt.xyz/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
         "SN": {
             "api_key": "6Z1tMrXzcYpIeSdGZueQWqb3BowlS6YuIoZLHe3dvIqkSfY0Ws5SHx78oGSTazG0jQ1agduSqe07FPPE8sdWTg",
@@ -97,12 +96,12 @@ config = {
         "HP" :{
             "api_key" : "HP",
             "announce_url" : "https://hidden-palace.net/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
         "ACM" :{
             "api_key" : "ACM api key",
             "announce_url" : "https://asiancinema.me/announce/customannounceurl",
-            # "anon" : "False",
+            # "anon" : False,
 
             # FOR INTERNAL USE ONLY:
             # "internal" : True,
@@ -125,22 +124,22 @@ config = {
             "pronfo_api_key" : "pronfo api key",
             "pronfo_theme" : "pronfo theme code",
             "pronfo_rapi_id" : "pronfo remote api id",
-            # "anon" : "False"
+            # "anon" : False
         },
         "LCD" : {
             "api_key" : "LCD api key",
             "announce_url" : "https://locadora.cc/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
         "LST" : {
             "api_key" : "LST api key",
             "announce_url" : "https://lst.gg/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
 	    "LT" : {
             "api_key" : "LT api key",
             "announce_url" : "https://lat-team.com/announce/customannounceurl",
-            # "anon" : "False"
+            # "anon" : False
         },
         "PTER" : {
             "passkey":'passkey',
